ac392fb1a14b283e0a5ad77ab118d948b7c86812
Jayed <137998593+jayeeed@users.noreply.github.com>
2023-11-14 13:16:07 +0600

    freezed

--- db/db_config.py
+++ db/db_config.py
@@ -7,6 +7,7 @@ client = MongoClient('mongodb+srv://xy3d:XgB8JVGTuWGd50kp@cluster0.20iimjx.mongo
 db = client['airbnb']
 properties_collection = db['properties']
 recommended_collection = db['recommended_properties']
+amenities_collection= db['amenities']
 
 def get_data():
     data_from_mongodb = properties_collection.find()
