58947cd8b10126faab89bbf5d2edb14ad8fbb2c3
bashar12345 <abul35-2002@diu.edu.bd>
2023-11-16 12:51:32 +0600

    init

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_pymongo import PyMongo
 from routes.recommend_routes import search_properties_route
 from routes.price_prediction_routes import price_predict_routes
 from routes.property_routes import property_routes
@@ -22,19 +21,9 @@ app.register_blueprint(verify_routes)
 
 
 
-app.config["MONGO_URI"] = "mongodb+srv://ipsita:Ipsita%402023@uk-bd0.u3pngqk.mongodb.net/airbnb"
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/price_prediction"
-# app.config["CORS_HEADERS"] = "Content-Type"
-# mongo = PyMongo(app)
-
- # MongoDB connection URI
-mongo = PyMongo(app)
-
-
 port = int(os.environ.get("PORT", 7050)) 
 
 if __name__ == '__main__':
-   
    app.run(debug=True, port=port, host='0.0.0.0')
 
 
