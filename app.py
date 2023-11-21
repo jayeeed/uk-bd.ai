2916ff6dfe2a11831ea989749209a0f80a476aad
Jayed Bin Jahangir <137998593+jayeeed@users.noreply.github.com>
2023-11-21 12:17:27 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from routes.property_routes import property_routes
 
 app = Flask(__name__)
 
+CORS(app)
+
 app.register_blueprint(search_properties_route)
 app.register_blueprint(property_routes)
+app.register_blueprint(price_predict_routes)
+app.register_blueprint(verify_routes)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
