d8b2264bafa82aaa4e52a2aa357dc238125b3b7c
John Doe <abul35-2002@diu.edu.bd>
2024-01-02 22:51:42 +0600

    just cleared the jubk codes

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask
-from routes.voice_search_routes import voice_search_route
 from routes.recommend_routes import search_properties_route
-from routes.property_routes import property_routes
-from routes.price_prediction_routes import price_predict_routes
-from routes.instant_varification_routes import verify_routes
+
 
 app = Flask(__name__)
 
 app.register_blueprint(search_properties_route)
-app.register_blueprint(property_routes)
-app.register_blueprint(price_predict_routes)
-app.register_blueprint(verify_routes)
-app.register_blueprint(voice_search_route)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
